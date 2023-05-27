ac7e9fb66d13dc7c574fd0826ea71f65d4edd234
Sanak <70244799+sanak-17@users.noreply.github.com>
2023-05-27 19:24:56 +0530

    Update match.py
    wrote a main function

--- match.py
+++ match.py
@@ -27,27 +27,31 @@ def match_pdf_to_paragraphs(pdf_file, paragraphs):
 
     similarity_scores = cosine_similarity(pdf_vector, paragraph_vectors)
 
-    #matched_paragraph_indices = similarity_scores.argmax(axis=1)
-
-    #matched_paragraphs = [paragraphs[i] for i in matched_paragraph_indices]
+   
 
     return similarity_scores
 
-pdf_file_path = '/content/ULLAS U.pdf'
+
+def main():
+    # Your code here to generate or retrieve the list of elements
+    
+    pdf_file_path = '/content/ULLAS U.pdf'
+    with open('/content/text.json', 'r') as f:
+        json_data = json.load(f)
+
+    data = json_data['data']
+    paragraphs= []
+    for i in range(len(data)):
+        paragraphs.append(data[i]['job_description']) 
+
+
+    matched_paragraphs = match_pdf_to_paragraphs(pdf_file_path, paragraphs)    
+
+    return matched_paragraphs
+
+if __name__ == '__main__':
+    result = main()
+    
 
 
 
-with open('/content/text.json', 'r') as f:
-  json_data = json.load(f)
-
-data = json_data['data']
-paragraphs= []
-for i in range(len(data)):
-    paragraphs.append(data[i]['job_description']) 
-
-
-    matched_paragraphs = match_pdf_to_paragraphs(pdf_file_path, paragraphs)
-
-print("Matched paragraphs:")
-for paragraph in matched_paragraphs:
-    print(paragraph)
