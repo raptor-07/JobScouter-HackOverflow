f082e85752c9ce71daaf7ac21e99f4efb9fa8bf5
PawanAK <pawanajjark@gmail.com>
2023-05-27 14:36:39 +0530

    updated model files

--- Backend/Controller/linkedInController.js
+++ Backend/Controller/linkedInController.js
@@ -1,2 +1,22 @@
 //call rrapid api
 //store data
+
+const getData = require("./getdata");
+const format = require("./format");
+const writeFile = require("./filewrite");
+
+async function linkedinController(req, res) {
+  try {
+    const jobData = await getData();
+    const csvString = format(jobData);
+    await writeFile("job-data.csv", csvString);
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment; filename=job-data.csv");
+    res.send(csvString);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+}
+
+module.exports = linkedinController;
--- Backend/Model/format.js
+++ Backend/Model/format.js
@@ -1,1 +1,10 @@
 //json -> csv
+const { Parser } = require("json2csv");
+
+function format(data) {
+  const fields = ["title", "company", "location", "description"];
+  const json2csvParser = new Parser({ fields });
+  return json2csvParser.parse(data);
+}
+
+module.exports = format;
