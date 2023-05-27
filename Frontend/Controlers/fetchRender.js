3fee20712aa3033cbca1abde925be42202129cc0
PawanAK <pawanajjark@gmail.com>
2023-05-28 02:05:37 +0530

    FPdf complete

--- Frontend/Controlers/fetchRender.js
+++ Frontend/Controlers/fetchRender.js
@@ -1,30 +1,38 @@
+let selectedFile;
 const fileInput = document.getElementById("pdfFileInput");
+fileInput.addEventListener("change", function (event) {
+  selectedFile = event.target.files[0];
+  console.log(selectedFile); // Output: File object representing the selected PDF file
+});
 
-document
-  .querySelector(".c-form__button")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
+document.querySelector(".button-54").addEventListener("click", function (e) {
+  console.log("hello");
 
-    const jsonData = {
-      file: fileInput.files[0],
-      description: document.querySelector(".c-form__input").value,
-    };
+  const description = document.querySelector(".c-form__input").value;
+  console.log(description);
 
-    console.log(jsonData);
-    fetch(" http://localhost:5000/BE1", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(jsonData),
+  const data = {
+    pdfFile: selectedFile,
+    description: description,
+  };
+
+  const jsonData = JSON.stringify(data);
+  console.log(jsonData);
+
+  fetch("http://localhost:5000/BE1", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonData,
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
