ccd02eb20743a19dfd89ac633a45931a6c1da3c6
PawanAK <pawanajjark@gmail.com>
2023-05-28 07:28:47 +0530

    chnages

--- Frontend/Controlers/fetchRender.js
+++ Frontend/Controlers/fetchRender.js
@@ -43,33 +43,32 @@ document
       });
   });
 
-  //To get the description and details from backend
+//To get the description and details from backend
 function render(apiData) {
-    var bodyElement = document.body;
-    bodyElement.innerHTML = "";
+  var bodyElement = document.body;
+  bodyElement.innerHTML = "";
 
-    const template= `<div class="widget">
-    <h1>Job Description</h1>
-    <p>${job}</p>
-    
-    <div class="CTA">
-      <h2>Company Name</h2>
-    <p>${as}</p>
-    
-    <h2>Location</h2>
-    <p>${as}</p>
-    
-    <h2>Job Type</h2>
-    <p>${asd}</p>
-    
-    <h2>Accuracy</h2>
-    <p class="value" count=${count}>0</p>
-    
-    <a href="apply.html" class="apply-btn">Apply Now</a>
-    </div>
-    </div>`
-    document.getElementsByClassName('body') = "${template}";
+  console.log(apiData);
 
+  // const template= `<div class="widget">
+  // <h1>Job Description</h1>
+  // <p>${job}</p>
+
+  // <div class="CTA">
+  //   <h2>Company Name</h2>
+  // <p>${as}</p>
+
+  // <h2>Location</h2>
+  // <p>${as}</p>
+
+  // <h2>Job Type</h2>
+  // <p>${asd}</p>
+
+  // <h2>Accuracy</h2>
+  // <p class="value" count=${count}>0</p>
+
+  // <a href="apply.html" class="apply-btn">Apply Now</a>
+  // </div>
+  // </div>`
+  // document.getElementsByClassName('body') = "${template}";
 }
-
-
